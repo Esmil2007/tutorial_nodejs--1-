16f1bd7305795a0b6ab834bcd10b9700966b29bd
Esmil2007 <kaikesmil13@gmail.com>
2025-05-05 15:36:17 -0300

    banco_de_dados

--- tutorial_nodejs/app.js
+++ tutorial_nodejs/app.js
@@ -6,9 +6,24 @@ const app = express(); // Armazena as chamadas e propriedades da biblioteca EXPR
 
 const PORT = 8500;
 
+//conexão com baco de dados 
+const db = new sqlite3.Database("users.db");
+
+db.serialize(() => {
+    db.run(
+        "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)"
+    )
+
+});
+
+
+
 app.use('/static', express.static(__dirname + '/static'));
 
+// configuração express para processar POST com BODY PARAMETERS 
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+
 
 app.get("/", (req, res) => {
     console.log("GET /");
@@ -23,11 +38,59 @@ app.get("/cadastro", (req, res) => {
     res.render("pages/cadastro");
     console.log("GET /cadastro")
 });
+app.post("/cadastro", (req, res) => {
+    //res.render("pages/dashboard");
+    const { username, password } = req.body;
+    // 1. Verficar se o usuario existe
+    const query1 = "SELECT * FROM users WHERE username=?"
+    const query2 = "INSERT INTO users (username, password) values (?, ?)"
+    db.get(query1, [username], (err, row) => {
 
+        if (err) throw err;
+
+        if (row) {
+            res.send("usuario ja existe");
+        }
+        else {
+            db.get(query2, [username, password], (err, row) => {
+                if (err) throw err;
+
+                console.log(JSON.stringify(row))
+                res.redirect("/login");
+            })
+        }
+    })
+
+})
 app.get("/login", (req, res) => {
     res.render("pages/login");
     console.log("GET /login")
 });
+app.post("/login", (req, res) => {
+    //res.render("pages/dashboard");
+    const { username, password } = req.body;
+    // 1. Verficar se o usuario existe
+    const query = "SELECT * FROM users WHERE username=? AND password=?"
+    db.get(query, [username, password], (err, row) => {
+        if (err) throw err;
+
+        console.log(JSON.stringify(row))
+        if (row) {
+            res.redirect("/dashboard");
+        } else {
+            res.send("usuário invalido")
+        }
+
+
+    })
+    // 2. se existir o usuario existir e a senha for válida BD, executar processo de login
+
+    // 3. se não, executar processo de negação de login
+
+    console.log("POST /login")
+    console.log(JSON.stringify(req.body));
+});
+
 app.get("/dashboard", (req, res) => {
     res.render("pages/dashboard")
     console.log("GET /dashboard")
@@ -35,6 +98,8 @@ app.get("/dashboard", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Servidor sendo executado na porta ${PORT}`);
 });
+
+
 app.get("/static", (req, res) => {
     res.send(`Você está na página static!`);
     console.log("GET /static");
